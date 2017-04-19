ee9c64333cebe7c6f75891f2f689f7cbbbcd3cb9
baymax <baymax1298@gmail.com>
2017-04-19 15:28:21 +0530

    first

---
+++ pro.js
@@ -0,0 +1,15 @@
+var express = require("express"),
+    app     = express(),
+    ejs     = require("ejs"); 
+
+app.use(express.static(__dirname + '/Public'));
+
+app.get("/", function(req, res){
+    
+    res.render("/index.ejs");
+    
+});
+
+app.listen(process.env.PORT || 8000, function(){
+    console.log("started");
+ });
